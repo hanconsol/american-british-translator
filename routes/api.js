2d7b9efa364792902ca2a1517cdb6f199db80306
hanconsol <hasfaraj49@gmail.com>
2024-10-25 05:41:30 -0500

    feat: add handle errors

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,28 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
+
   const translator = new Translator();
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      console.log(req.body);
+      const { text, locale } = req.body;
+
+      if (text === "" || text === " ") {
+        res.send({ error: 'No text to translate' });
+        return;
+      };
+
+      if (!text || !locale) {
+        res.send({ error: 'Required field(s) missing' })
+        return;
+      };
+
+      if (locale !== "american-to-British" || locale !== "british-to-american") {
+        res.send({ error: 'Invalid value for locale field' });
+        return;
+      };
+      res.send({ text: req.body.text, translation: "coming soon" })
     });
 };
